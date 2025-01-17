5261472611e816e6595a84401ae1a11e84e26b4d
Krzysztof Czerenko <krzysztofczerenko@gmail.com>
2025-01-17 14:37:56 +0100

    Calender component and dateService added

---
+++ src/app/components/calendar-components/availability/availability.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-availability',
+  imports: [],
+  templateUrl: './availability.component.html',
+  standalone: true,
+  styleUrl: './availability.component.css'
+})
+export class AvailabilityComponent {
+
+}
